b564ceff0be8c65f41c9c7e5cb2513e30ba17d9d
jdluna <jhosep.dario@gmail.com>
2019-07-06 13:45:10 -0500

    Adds script validation for errors

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 dockers run node:7-alpine node --version
+if [ $? != 0 ]; then
+  exit 1
+fi
+
 docker run node:6-alpine node --version
+if [ $? != 0 ]; then
+  exit 1
+fi
+
 docker run -v $HOME/.m2:/root/.m2 -v $WORKSPACE:/apps/ maven:3-alpine ls -la /apps
 
